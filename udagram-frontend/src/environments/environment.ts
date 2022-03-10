44289865da299881c6fff08ea15b2236ba4ce3a6
Sherouk Rashed <sherouk.mohammed.rashed@gmail.com>
2022-03-10 16:48:58 +0200

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-ncqucfci.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udacity-udagram.eba-ncqucfci.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
